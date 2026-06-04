543a18060a05e6e5859d140f3b334f0ae1b13486
Tilak Sahani <89787328+lordcommandertilak@users.noreply.github.com>
2026-06-04 16:40:16 +0530

    variable and data type lesson

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144526
+let accountEmail = "tilak@google.com"
+var accountPassword = "1234"
+
+accountCity = "Guwahati" 
+let accountState;
+// not to use just name variable. use "let vanirableName"
+
+// accountId = 2;
+/*
+Prefere not to use "var"
+bracouse of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState]);
---
+++ 02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer JS/version 
+
+//alert("hello") // we are using node.js, not browser
+
+console.log(3+3)
+console.log("Hello World")
+
+let name = "tilak" // string
+let age = 30 // number
+let isloggedIn = false // boolean
+
+let state = null 
+
+// number => 2 to power 53
+// bigint 
+// string '' or ""
+// boolen = true/false
+// null  = it is stand alone value 
+// undefined = 
+// symbol = uniqueness
+
+
+// object 
+
+console.log(typeof "kjskdjkj")
+console.log(typeof age)
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
